25852336f7ee1b20dcdc1baf16bd3448dfcdc317
unknown <SENA@MOSAPRCBAFSP290.FormacionSena.RED>
2023-07-27 09:20:11 -0500

    version TO

--- tarjetaObservacion/index.php
+++ tarjetaObservacion/index.php
@@ -1,70 +1,101 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="../css/style_TO.css">
-        <title>Tarjeta de Observación</title>
-    </head>  
-    <body>
-        <div class="contenedor">
-        
-            <form action="php/carga.php" method="POST">
+<head>
+    <title>Tarjeta de Observación</title>
+    <link rel="stylesheet" href="../css/style_TO.css">
+</head>
+<body>
+    <header>
+    <a href="../php/rolPersona/indexPersona.php"><img src="../img/LogoSena.png" alt="logosena"></a>
+    <h1>Tarjeta de Observación del Entorno</h1>
+    </header>
+
+    <div class="contenedor">
+        <form action="php/carga.php" method="POST" id="formulario">
             
-            <h1>Tarjeta de Observacion del entorno</h1>
-
-            <h3>TIPO DE SITUACIÓN</h3>
-            <b>Acto inseguro:</b> Acciones y decisiones humanas, que pueden causar una situación insegura, accidente o enfermedad, 
-                    con consecuencias para el trabajador, la producción, el medio ambiente y/u otras personas <br>
-
-            <b>Condición insegura: </b> Es todo un equipo, materia prima, herramientas, máquinas, instalaciones o el medio ambiente
-                    que se convierte en un peligro para las personas, los bienes, la operación que bajo determinadas condiciones puede generar un accidente o una enfermedad.<br>
-
-            <b>Incidente de Trabajo:</b> Suceso sucedido en el trabajo , que tuvo el potencial de ser un accidente, en el que hubo 
-            personas involucradas sin que sufrieran lesiones o se presentaran daño a la propiedad y/o pérdida de los procesos.<br>
-
-            <b>Incidente Ambiental: </b> Evento que bajo condiciones no controladas puede llevar a un accidente ambiental generando 
-            pérdidas e impactos negativos sobre uno o varios elementos del Medio Ambiente.<br><br>
+            <div class="seccion">
+                <h2>TIPO DE SITUACIÓN</h2>
+                <ul>
+                    <li><b>Acto inseguro:</b> Acciones y decisiones humanas, que pueden causar una situación insegura, accidente o enfermedad,
+                        con consecuencias para el trabajador, la producción, el medio ambiente y/u otras personas.</li>
+                    <li><b>Condición insegura:</b> Es todo un equipo, materia prima, herramientas, máquinas, instalaciones o el medio ambiente
+                        que se convierte en un peligro para las personas, los bienes, la operación que bajo determinadas condiciones puede generar un accidente o una enfermedad.</li>
+                    <li><b>Incidente de Trabajo:</b> Suceso sucedido en el trabajo, que tuvo el potencial de ser un accidente, en el que hubo
+                    personas involucradas sin que sufrieran lesiones o se presentaran daño a la propiedad y/o pérdida de los procesos.</li>
+                    <li><b>Incidente Ambiental:</b> Evento que bajo condiciones no controladas puede llevar a un accidente ambiental generando
+                        pérdidas e impactos negativos sobre uno o varios elementos del Medio Ambiente.</li>
+                </ul>
+            </div>
             <hr>
-            <h3>DATOS GENERALES</h3>
 
-            Dirección General/Regional: <input type="text" name="regional" required>
-            Despacho Regional/Centro de Formación: <input type="text" name="centro"  required>
-            Dirección de la Sede o subsede: <input type="text" name="direccion"  required>
-            Ciudad: <input type="text" name="ciudad"  required>
-            <br>
-            Lugar de identificacion de la situación: <input type="text" name="lugar" required>
-            Fecha:<input type="date" name="fecha" class="f" required>
-            <br>
-            Nombre de quien reporta: <input type="text" name="remitente"  required>
-            Cargo: <input type="text" name="cargo"  required>         
+            <div class="seccion">
+                <h2>DATOS GENERALES</h2>
+                <div class="datos-generales">
+                    <div>
+                        <label for="regional">Dirección General/Regional:</label>
+                        <input type="text" name="regional" id="regional" required>
+                    </div>
+                    <div>
+                        <label for="centro">Despacho Regional/Centro de Formación:</label>
+                        <input type="text" name="centro" id="centro" required>
+                    </div>
+                    <div>
+                        <label for="direccion">Dirección de la Sede o subsede:</label>
+                        <input type="text" name="direccion" id="direccion" required>
+                    </div>
+                    <div>
+                        <label for="ciudad">Ciudad:</label>
+                        <input type="text" name="ciudad" id="ciudad" required>
+                    </div>
+                    <div>
+                        <label for="lugar">Lugar de identificación de la situación:</label>
+                        <input type="text" name="lugar" id="lugar" required>
+                    </div>
+                    <div>
+                        <label for="fecha">Fecha:</label>
+                        <input type="date" name="fecha" id="fecha" class="f" required>
+                    </div>
+                    <div>
+                        <label for="remitente">Nombre de quien reporta:</label>
+                        <input type="text" name="remitente" id="remitente" required>
+                    </div>
+                    <div>
+                        <label for="cargo">Cargo:</label>
+                        <input type="text" name="cargo" id="cargo" required>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="seccion">
+                <h2>TIPO DE SITUACIÓN</h2>
+                <div class="radio-buttons">
+                    <input type="radio" id="1opcion" name="tipo" value="Acto Inseguro" required>
+                    <label for="1opcion">Acto Inseguro</label><br>
+                    <input type="radio" id="2opcion" name="tipo" value="Condición Insegura" required>
+                    <label for="2opcion">Condición Insegura</label><br>
+                    <input type="radio" id="3opcion" name="tipo" value="Incidente De Trabajo" required>
+                    <label for="3opcion">Incidente De Trabajo</label><br>
+                    <input type="radio" id="4opcion" name="tipo" value="Incidente Ambiental" required>
+                    <label for="4opcion">Incidente Ambiental</label><br>
+                </div>
+            </div>
 
+            <div class="seccion">
+                <h2>DESCRIPCIÓN DE LA SITUACIÓN</h2>
+                <textarea id="descripcion" name="descripcion" class="descripcion" required></textarea>
+            </div>
 
-            <br><br>
-            <h3>TIPO DE SITUACIÓN</h3>
-            <h4>Marque la opcion que corresponda</h4>
-            <input type="radio" id="1opcion" name="tipo" value="Acto Inseguro" required><label for="1opcion">Acto Inseguro</label>
-            <input type="radio"  id="2opcion" name="tipo" value="Condición Insegura"  required><label for="2opcion">Condición Insegura</label>
-            <input type="radio"  id="3opcion" name="tipo" value="Incidente De Trabajo" required> <label for="3opcion">Incidente De Trabajo</label>
-            <input type="radio"  id="4opcion" name="tipo" value="Incidente Ambiental" required><label for="4opcion">Incidente Ambiental</label>
- 
- 
-            <br><br>
-            <h3>DESCRIPCIÓN DE LA SITUACIÓN</h3>
-            <label>
-            Describa detalladamente la situación observada, indicado si la persona involucrada es empleado 
-                o visitante, relacionando las consecuencias que puedan generarse <br>
-            <input type="text" id="descripcion" name="descripcion" class="descripcion" required>
-            </label>
-            <br>
-            <h3>IMAGEN</h3>
-            <label for="imagen">Adjuntar imagen:</label>
-            <label>
-                Por favor adjunte una imagen de la situacion que anteriormente describio (En caso de no tener imagen por favor omita este campo).
-            </label>
-            <br><br>
-            <input type="file" id="imagen" name="imagen">
-            <br><br>
-            <input type="submit">
-            </form>
-        </div>
-    </body>
+            <div class="seccion">
+                <h2>IMAGEN</h2>
+                <label for="imagen">Adjuntar imagen:</label>
+                <p>Por favor adjunte una imagen de la situación que anteriormente describió (En caso de no tener imagen por favor omita este campo).</p>
+                <input type="file" id="imagen" name="imagen">
+            </div>
+            
+            <div class="submit-button">
+                <input type="button" value="Enviar" class="Enviar">
+            </div>
+        </form>
+    </div>
+</body>
 </html>
--- tarjetaObservacion/php/carga.php
+++ tarjetaObservacion/php/carga.php
@@ -1,23 +1,46 @@
 <?php
-    include "../../db/conexion.php";
-    $regional=$_POST['regional'];
-    $centro=$_POST['centro'];
-    $direccion=$_POST['direccion'];
-    $ciudad=$_POST['ciudad'];
-    $lugar=$_POST['lugar'];
-    $fecha=$_POST['fecha'];
-    $remitente=$_POST['remitente'];
-    $cargo=$_POST['cargo'];
-    $tipo=$_POST['tipo'];
-    $descripcion=$_POST['descripcion'];
-    $consul="INSERT INTO `registros`(`id`, `regional`, `centro`, `direccion`, `lugar`, `fecha`, `nombre`, `cargo`, `tipo`, `descripcion`) 
-                VALUES ('$regional','$centro','$direccion','$ciudad','$lugar','$fecha','$remitente','$cargo','$tipo','$descripcion') ";
-    $resultado = mysqli_query($conexion,$consul);
-    if ($resultado) {
-        ?>
-        <h3 class="ok">se ha registrado correctamente</h3>
-        <?php
-    } else{
-        echo" <h1>error al intentar insertar datos</h1>";
-    }
-?>
+            if ($_SERVER["REQUEST_METHOD"] === "POST") {
+                // Verificar si se ha hecho clic en el botón "Enviar"
+                if (isset($_POST["Enviar"])) {
+                    // Obtener los datos del formulario
+                    $diregional = $_POST["diregional"];
+                    $desregional = $_POST["desregional"];
+                    $dirsede = $_POST["dirsede"];
+                    $ciudad = $_POST["ciudad"];
+                    $lugariden = $_POST["lugariden"];
+                    $fecha = $_POST["fecha"];
+                    $nomreporta = $_POST["nomreporta"];
+                    $cargo = $_POST["cargo"];
+                    $actinseguro = $_POST["actinseguro"];
+                    $coninsegura = $_POST["coninsegura"];
+                    $inctrabajo = $_POST["inctrabajo"];
+                    $incambiental = $_POST["incambiental"];
+                    $desituacion = $_POST["desituacion"];
+                    $imagen = $_POST["imagen"];
+                    // (Agrega aquí los campos restantes del formulario)
+
+                    // Conectar a la base de datos (Asegúrate de cambiar los datos de conexión)
+                    $host = "localhost";
+                    $username = "";
+                    $password = "";
+                    $database = "tobservacion";
+
+                    $conn = new mysqli($host, $username, $password, $database);
+
+                    if ($conn->connect_error) {
+                        die("Error al conectar a la base de datos: " . $conn->connect_error);
+                    }
+
+                    // Preparar la consulta SQL para insertar los datos en la base de datos
+                    $sql = "INSERT INTO `tobservacion`(`diregional`, `desregional`, `dirsede`, `ciudad`, `lugariden`, `fecha`, `nomreporta`, `cargo`, `actinseguro`, `coninsegura`, `inctrabajo`, `incambiental`, `desituacion`, `imagen`) 
+                            VALUES ('$diregional', '$desregional', '$dirsede', '$ciudad', '$lugariden', '$fecha', '$nomreporta', '$cargo', '$actinseguro', '$coninsegura', '$inctrabajo', '$incambiental', '$desituacion', '$imagen ')";
+
+                    if ($conn->query($sql) === TRUE) {
+                        echo "Formulario enviado correctamente.";
+                    } else {
+                        echo "Error al enviar el formulario: " . $conn->error;
+                    }
+                    $conn->close();
+                }
+            }
+            ?>
