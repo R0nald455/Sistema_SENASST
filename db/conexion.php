eaa7610e3aa4ef9024cdf76c109678dafb8f0f27
JesusCuadro <jesuscuadro190@gmail.com>
2023-09-05 19:41:57 -0500

    conexion del host

--- db/conexion.php
+++ db/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$server="localhost:3307";
-$usuario="root";
+$server="localhost";
+$usuario="u632157300_SSTCBA";
 $password="";
-$database="sstcba";
+$database="u632157300_SSTCBA";
 $conexion=new mysqli($server, $usuario, $password, $database);
 if(mysqli_connect_error()){
 	echo 'Error, no se pudo conectar a la base de datos: '.mysqli_connect_error();
