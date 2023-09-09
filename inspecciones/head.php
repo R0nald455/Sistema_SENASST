e617ca8cf26a767d4365f04ea134d751bf7adc41
JesusCuadro <jesuscuadro190@gmail.com>
2023-09-09 13:56:21 -0500

    landing camillas terminada

--- inspecciones/head.php
+++ inspecciones/head.php
@@ -1,4 +1,4 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Inspección de extintores</title>
 
