bf392839dbb17cb5d70e23a4fddf80f0aaf65576
JesusCuadro <jesuscuadro190@gmail.com>
2023-09-16 09:06:32 -0500

    cambio en index administrador

--- php/rolFuncionario/indexadministrador.php
+++ php/rolFuncionario/indexadministrador.php
@@ -48,9 +48,7 @@ require_once("../../db/conexion.php");
 						<li><a onclick="window.location.href='../../reglamento/index.php'">Reglamento</a></li>
 						<li><a href="#">Modulos administrativos</a>
 							<ul> <b>
-									<li><a onclick="window.location.href='../../reglamento/index.php'">Reglamento</a></li>
-
-									<li><a onclick="window.location.href='../../administrador/view/user.php'">Administrar
+									<li><a href="../../administrador/View/user.php">Administrar
 											usuarios</a></li>
 									<li><a onclick="window.location.href='../../contenidos/index.php'">Administrar
 											Contenidos</a></li>
