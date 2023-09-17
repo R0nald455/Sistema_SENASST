b2f1fb01240929e4ffe2cefededa3bd65436a2b9
JesusCuadro <jesuscuadro190@gmail.com>
2023-09-16 23:16:46 -0500

    conexiones

--- db/conexionPDO.php
+++ db/conexionPDO.php
@@ -9,10 +9,10 @@ $db       = "u632157300_SSTCBA";
 
 */
 
-$server   = "localhost:3307";
-$user     = "root";
-$pwd      = "";
-$db       = "sstcba";
+$server   = "localhost";
+$user     = "u632157300_SSTCBA";
+$pwd      = "SENAsst2023**";
+$db       = "u632157300_SSTCBA";
 
 try {
     $conexion = new PDO("mysql:host=$server;dbname=$db", $user, $pwd);
