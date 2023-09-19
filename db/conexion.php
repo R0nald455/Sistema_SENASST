97e3b8e80a53d25eb0d4abe610c2dd18cf58eccb
JesusCuadro <jesuscuadro190@gmail.com>
2023-09-19 10:47:13 -0500

    conexiones

--- db/conexion.php
+++ db/conexion.php
@@ -9,10 +9,10 @@ $database="u632157300_SSTCBA";
 
 */
 
-$server="localhost:3307";
-$usuario="root";
-$password="";
-$database="sstcba";
+$server="localhost";
+$usuario="u632157300_SSTCBA";
+$password="SENAsst2023**";
+$database="u632157300_SSTCBA";
 
 $conexion=new mysqli($server, $usuario, $password, $database);
 if(mysqli_connect_error()){
