fb509dcfb27923a72419dc49871246db93791297
JesusCuadro <jesuscuadro190@gmail.com>
2023-09-22 12:12:36 -0500

    conex

--- db/conexion.php
+++ db/conexion.php
@@ -1,22 +1,22 @@
 <?php
-// $server="localhost";
-// $usuario="u632157300_SSTCBA";
-// $password="SENAsst2023**";
-// $database="u632157300_SSTCBA";
+$server="localhost";
+$usuario="u632157300_SSTCBA";
+$password="SENAsst2023**";
+$database="u632157300_SSTCBA";
 
-// $conexion=new mysqli($server, $usuario, $password, $database);
-// if(mysqli_connect_error()){
-// 	echo 'Error, no se pudo conectar a la base de datos: '.mysqli_connect_error();
-// }
-
-$servidor= "localhost:3307";
-$usuario= "root";
-$password = "";
-$nombreBD= "sstcba";
-$conexion = new mysqli($servidor, $usuario, $password, $nombreBD);
-if ($conexion->connect_error) {
-    die("la conexión ha fallado: " . $conexion->connect_error);
+$conexion=new mysqli($server, $usuario, $password, $database);
+if(mysqli_connect_error()){
+	echo 'Error, no se pudo conectar a la base de datos: '.mysqli_connect_error();
 }
+
+// $servidor= "localhost:3307";
+// $usuario= "root";
+// $password = "";
+// $nombreBD= "sstcba";
+// $conexion = new mysqli($servidor, $usuario, $password, $nombreBD);
+// if ($conexion->connect_error) {
+//     die("la conexión ha fallado: " . $conexion->connect_error);
+// }
 if (!$conexion->set_charset("utf8")) {
     printf("Error al cargar el conjunto de caracteres utf8: %s\n", $conexion->error);
     exit();
